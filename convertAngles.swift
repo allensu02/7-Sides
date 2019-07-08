ee41d2977d113307ae2d345c2093eff567501ee1
Allen Su <37977270+allensu02@users.noreply.github.com>
2019-07-08 16:15:20 -0700

    Add files via upload

---
+++ convertAngles.swift
@@ -0,0 +1,16 @@
+//
+//  convertAngles.swift
+//  7 Sides
+//
+//  Created by Student on 7/12/18.
+//  Copyright © 2018 dmaadmin. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+//180 degrees * pi= radian
+//radian = 180/pi
+func convertDegreesToRadians (degrees: CGFloat) -> CGFloat {
+    return (degrees * 0.0174533)
+    
+}
